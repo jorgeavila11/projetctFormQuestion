e7085d9059dc0be079b8be769dac40e82e002101
Jorge Ávila <jorgeavila11@hotmail.com>
2021-06-07 22:26:36 -0300

    Inserindo opcoes nos cards

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-
+var idContador = 0;
+var idFormulario = 0;
 const questions = [
     {
         question:'Quanto é 2 + 2 ?',
@@ -17,84 +18,88 @@ const DOM = {
     cardConteiner : document.querySelector('.main'),
     
     createCard(){
-        const card = `
+        idContador++;        
+        var idCard = "cardExtra"+idContador;        
+        var card = `
             <div class="card">
-                <form action="#">
+                <form action="" onsubmit="Form.submit(event)">
                     <div>
                         <h3>Pergunta ?</h3>
                             <textarea cols="50" rows="1" placeholder="Faça uma pergunta">faça uma pergunta</textarea>
                     </div>
                     <div class="divtable">
-                        <table class="table">
+                        <table class="table${idCard}">
                             <tbody>
                                 <div>
                                     dentro
                                 </div>
                             </tbody>
                         </table>
-                        <button>Adicionar opção</button>
-                    </div>                    
+                        <button id=${idCard} onclick="adicionaForm(${idCard})">Adicionar opção</button>
+                    </div> 
                 </form>
             </div>
         `
         return card;
     },
 
-    // createForm(){        
-    //     const form = `        
-    //         <td><input type="radio" name="nome" id="nome"></td>
-    //         <td><input type="text" name="alternativa" id="alternativa" value="Opção"></td>
-    //         <td><button>X</button></td>
-    //     `
-    //     return form;
-    // },
+    
     addCard(){
         const newCard = document.createElement('div');
         newCard.innerHTML = DOM.createCard();
         DOM.cardConteiner.appendChild(newCard);
-    },    
-
-    // addForm(){
-    //     const tr = document.createElement('tr');
-    //     tr.innerHTML = DOM.createForm();
-    //     DOM.tableContainer.appendChild(tr); 
-    //     
-    // },
-    
-    removForm(){
-        DOM.tableContainer.innerHTML = "";
-    },    
+    },     
     
 }
 
-DOM.addCard();
-DOM.addCard();
-
-
-var tableContainer = document.querySelectorAll('table')[0];
-console.log(tableContainer);
-
-
-
-function createForm(){ 
-    const form = `        
-        <td><input type="radio" name="nome" id="nome"></td>
-        <td><input type="text" name="alternativa" id="alternativa" value="Opção"></td>
-        <td><button>X</button></td>
-    `
-    return form;
-};
-
-function addForm(){
-    const tr = document.createElement('tr');
-    tr.innerHTML = createForm();    
-    tableContainer.appendChild(tr);
+const DOMForm = { 
+    
+    //tableContainer : document.querySelectorAll('table tbody')[0],    
+    
+    createForm(){
+        
+        const form = `        
+            <td><input type="radio" name="nome" id="nome"></td>
+            <td><input type="text" name="alternativa" id="alternativa" value="Opção"></td>
+            <td><button>X</button></td>
+            `
+        return form;
+    },
+    
+    addForm(e){
+        const tr = document.createElement('tr');
+        const el = document.getElementsByClassName("table"+e)[0];
+        tr.innerHTML = DOMForm.createForm();
+        el.appendChild(tr);
+        
+        console.log(el);
+        
+        // const tr = document.createElement('tr');        
+        // tr.innerHTML = DOMForm.createForm();
+        // DOMForm.tableContainer.appendChild(tr);        
+    }
 }
 
-addForm();
-addForm();
-addForm();
-addForm();
+const Form = {
+    submit(event){
+        event.preventDefault();
+    },
+}
+
+function adicionarCard(){    
+    DOM.addCard();
+}
+
+function adicionaForm(e){    
+    DOMForm.addForm(e.id);
+}
+
+
+
+
+
+
+
 
 
 
